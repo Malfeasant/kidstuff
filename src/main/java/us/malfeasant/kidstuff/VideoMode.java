fdf29f769ac93d68b080c92840dbf5126ad96ec3
Mischa Sumrall <1212508+Malfeasant@users.noreply.github.com>
2023-06-26 10:46:40 -0700

    Added comments

--- src/main/java/us/malfeasant/kidstuff/VideoMode.java
+++ src/main/java/us/malfeasant/kidstuff/VideoMode.java
@@ -1,8 +1,8 @@
 package us.malfeasant.kidstuff;
 
 public enum VideoMode {
-    TEXT,   // 8-bit char pointer + 8-bit color (bg/fg)
-    QUAD,   // 4-bit x 4 quads = 16-bit (use char ptr + color)
-    NYBBLEMAP,  // 4-bit per pixel = 32-bit per char per line
-    HICOLOR;    // 16-bit per pixel = 128-bit per char per line
+    TEXT,   // 8-bit char pointer + 8-bit color (bg/fg) 78x52 total 8k, also needs 2k char gen
+    QUAD,   // 4-bit x 4 quads = 16-bit per block - total 8k
+    NYBBLEMAP,  // 4-bit per pixel = 32-bit per char per line - 624x416 = 128k
+    HICOLOR;    // 16-bit per pixel = 128-bit per char per line - 512k
 }
