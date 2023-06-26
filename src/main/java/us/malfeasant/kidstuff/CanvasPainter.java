8d6280b06740ff8b00bdbefa0165e4fd9067b1a5
Mischa Sumrall <1212508+Malfeasant@users.noreply.github.com>
2023-06-26 16:40:49 -0700

    More register bits

--- src/main/java/us/malfeasant/kidstuff/CanvasPainter.java
+++ src/main/java/us/malfeasant/kidstuff/CanvasPainter.java
@@ -36,6 +36,58 @@ public class CanvasPainter extends AnimationTimer {
                 return data;
             }
         },
+        SPLITLINE { // text line that divides upper from lower
+            @Override
+            void poke(CanvasPainter p, byte data) {
+                p.splitLine = data & 0x3f;
+            }
+            @Override
+            byte peek(CanvasPainter p) {
+                return (byte) p.splitLine;
+            }
+        },
+        UPPERPAGE {    // which 4k page to pull gfx data/text from
+            @Override
+            void poke(CanvasPainter p, byte data) {
+                p.upperPage = data & 0xff; // needed because there's no unsigned byte type...
+            }
+            @Override
+            byte peek(CanvasPainter p) {
+                return (byte) p.upperPage;
+            }
+        },
+        LOWERPAGE {    // 
+            @Override
+            void poke(CanvasPainter p, byte data) {
+                p.lowerPage = data & 0xff; // needed because there's no unsigned byte type...
+            }
+            @Override
+            byte peek(CanvasPainter p) {
+                return (byte) p.lowerPage;
+            }
+        },
+        CHARPAGE {    // lower 8 bits
+            @Override
+            void poke(CanvasPainter p, byte data) {
+                var page = p.charPage & 0x100;  // mask away lower bits
+                p.charPage |= (data & 0xff);    // replace lower bits
+            }
+            @Override
+            byte peek(CanvasPainter p) {
+                return (byte) (p.charPage & 0xff);
+            }
+        },
+        CHARPAGEHI {    // Only one bit - TODO rename if more bits come from other sources...
+            @Override
+            void poke(CanvasPainter p, byte data) {
+                var page = p.charPage & 0xff;  // mask away upper bit
+                p.charPage |= (data & 1) << 8;    // replace upper bit
+            }
+            @Override
+            byte peek(CanvasPainter p) {
+                return (byte) (p.charPage >> 8);
+            }
+        },
         // TODO offsets for registers
 
         BORDERCOLOR {
